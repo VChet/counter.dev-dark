f4a3d8bf98b45929bab2b70e49a3bffd5d8629a4
VChet <17050347+VChet@users.noreply.github.com>
2026-03-20 16:37:22 +0500

    chore: update dependencies

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,16 +1,11 @@
 export default {
-  extends: "stylelint-config-standard",
-  plugins: ["stylelint-prettier"],
+  extends: [
+    "stylelint-config-standard",
+    "@stylistic/stylelint-config"
+  ],
   rules: {
-    "prettier/prettier": [true, {
-      trailingComma: "none",
-      tabWidth: 2,
-      semi: true,
-      singleQuote: false,
-      printWidth: 120,
-      bracketSpacing: true,
-      endOfLine: "auto"
-    }],
+    "@stylistic/max-line-length": null,
+    "@stylistic/selector-list-comma-newline-after": "always-multi-line",
     "at-rule-no-vendor-prefix": null,
     "at-rule-empty-line-before": "never",
     "comment-empty-line-before": "never",
