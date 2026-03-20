55a4b8f995233662f2e3dc3481f5589f3d214bff
VChet <17050347+VChet@users.noreply.github.com>
2026-03-20 16:37:22 +0500

    chore: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,15 @@ import neostandard from "neostandard";
 
 export default [
   ...neostandard({
+    ts: false,
     noJsx: true,
     semi: true
   }),
   {
     rules: {
+      "@stylistic/brace-style": "off",
       "@stylistic/comma-dangle": ["error", "never"],
+      "@stylistic/operator-linebreak": ["error", "after"],
       "@stylistic/quotes": ["error", "double"],
       "@stylistic/space-before-function-paren": ["error", "never"]
     }
